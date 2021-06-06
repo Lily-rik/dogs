2ee4fca3bf27fa2078a484613d15799dfafd6e63
reiko-ishihara <violet794.py16@gmail.com>
2021-06-06 22:28:19 +0000

    [Controllers,Models,Routes Add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,42 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root 'homes#top' 
+  
+  scope module: :public do
+    
+    root 'homes#top' 
+    get 'search' => 'searches#search', as: 'search'
+    
+    # Users
+    resources :users, only: [:show, :edit, :update] do
+      # relationships
+      resource :relationships, only: [:create, :destroy]
+      get :follows, on: :member
+      get :followers, on: :member
+    end
+    get 'my_page/:id/edit' => 'users#edit_my_page', as: 'edit_my_page'
+    patch 'my_page/:id' => 'users#update_my_page', as: 'update_my_page'
+    get 'users/:id/unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
+    patch 'users/:id/withdrawal' => 'users#withdrawal', as: 'withdrawal'
+    get 'users/:id/favorites' => 'users#favorites', as: 'favorites'
+    
+    # posts
+    resources :posts, only: [:show, :new, :create, :edit, :update, :destroy] do
+      # comments
+      resources :comments, only: [:create, :destroy]
+      # favorites
+      resource :favorites, only: [:create, :destroy]
+    end
+    get 'posts/ranking' => 'posts#ranking', as: 'ranking'
+    
+  end
+  
+  
+  namespace :admin do
+    get 'top' => 'homes#top', as: 'top'
+    get 'search' => 'searches#search', as: 'search'
+    resources :users, only: [:index, :show, :edit, :update]
+  end
   
   
   
