8302e18aa024acc4b9d95247e3b5a43d7dd74b17
reiko-ishihara <violet794.py16@gmail.com>
2021-06-27 11:22:25 +0000

    [Notifications Update]

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,10 +5,10 @@ class Notification < ApplicationRecord
   
   belongs_to :post, optional: true
   belongs_to :comment, optional: true
+  belongs_to :chat, optional: true
   
   belongs_to :visitor, foreign_key: 'visitor_id', class_name: 'User', optional: true # optional: trueはnilを許可するもの
   belongs_to :visited, foreign_key: 'visited_id', class_name: 'User', optional: true # belongs_toはnilが許可されないが、今回はnilを設定したいため追加する
   
   
 end
-
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_27_011521) do
+ActiveRecord::Schema.define(version: 2021_06_27_093336) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 2021_06_27_011521) do
     t.boolean "checked", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "chat_id"
   end
 
   create_table "post_hashtags", force: :cascade do |t|
