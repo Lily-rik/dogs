59d520e29ebc70a7e13c50ae87b1b1e4d80b0f4e
reiko-ishihara <violet794.py16@gmail.com>
2021-07-23 09:50:32 +0000

    [Responsive Update]

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -7,7 +7,7 @@ class Public::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = Post.where(user_id: @user.id).page(params[:page]).reverse_order
+    @posts = Post.where(user_id: @user.id).includes(:user).page(params[:page]).reverse_order
   end
 
   # 会員情報の編集
